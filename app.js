d2a25ee0ca372f7d0260905e8299fc472be03278
tauqeerkhan199 <ktauqeer345@gmail.com>
2026-09-03 23:35:16 +0530

    first update

--- app.js
+++ app.js
@@ -11,13 +11,8 @@ const server = http.createServer((req, res) => {
 <<<<<<< HEAD
         <h1>Hello from Node.js team hello team \\\\ </h1>
 =======
-<<<<<<< HEAD
-        <h1>Hello from Node.js team !!!! hiii family </h1>
-=======
-<<<<<<< HEAD
-        <h1>Hello from Node.js team tauqeer khan new test !!!!</h1>
-=======
-        <h1>Hello from Node.js team tauqeer khan ||||||| </h1>
+
+
 >>>>>>> 02dc6245c05cbbf0333d3daa0ad7560ee2fd02f8
 >>>>>>> de009aa85237db08226566eb3e4101db14df6fd5
 >>>>>>> 8f01d89d64dc6b9c46cb87953618041ba85bb52a
